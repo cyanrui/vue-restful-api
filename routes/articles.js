cc69133027be80a98facb64842b02d5f56e4291e
cyr <22222@qq.com>
2020-11-05 16:44:06 +0800

    vue接口

---
+++ routes/articles.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { findById } = require('../db/articleModel');
+const fs = require('fs')
+const path = require('path')
+let router = express.Router();
+var multiparty = require('multiparty');//处理文件上传
+let articleModel = require('../db/articleModel');
+
+router.post('/write',(req,res,next)=>{
+    // 接收post数据
+    let {title,content,username,id} = req.body;
+    // 当前时间
+    let createTime = Date.now()
+    if(id){
+        // 修改文章
+        id = new Object(id);
+        articleModel.updateOne({_id:id},{
+            title,content,createTime,username
+        }).then(data=>{
+            res.json({
+                error:0,
+                data
+            })
+        }).catch(err=>{
+            res.json({
+                error:1
+            })
+        })
+    }else{
+        // 新增文章
+        // 插入数据库
+        articleModel.insertMany({
+            username,
+            title,
+            content,
+            createTime
+        }).then(data=>{
+            res.json({
+                error:0,
+                data
+            })
+        }).catch(err=>{
+            res.json({
+                error:1
+            })
+        })
+    }
+})
+
+
+router.get('/delete',(req,res,next)=>{
+    let id = req.query.id;
+    id = new Object(id);
+    // 删除
+    articleModel.deleteOne({_id:id})
+    .then(data=>{
+        res.json({
+            error:0
+        })
+    })
+    .catch(err=>{
+        res.json({
+            error:1
+        })
+    })
+})
+
+
+module.exports = router;
